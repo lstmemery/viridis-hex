8fa51af7d0d4696a8e7e88b22fa500ff2173ab72
Matthew Emery <m.emery@alumni.ubc.ca>
2016-12-19 15:58:22 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='viridis-hex',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/lstmemery/viridis-hex',
+    license='MIT',
+    author='lstmemery',
+    author_email='matthew.emery44@gmail.com',
+    description='Get the Viridis Colormap in Hexcode'
+)
