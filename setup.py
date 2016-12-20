7f7553a9c3c7ca5874a28374e21a609f6939c304
Matthew Emery <m.emery@alumni.ubc.ca>
2016-12-19 16:28:38 -0800

    Added requirements to proper part of setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from distutils.core import setup
 setup(
     name='viridis-hex',
     version='0.1',
-    packages=['pytest', 'matplotlib'],
+    packages=[''],
     url='https://github.com/lstmemery/viridis-hex',
     license='MIT',
     author='lstmemery',
+    requires=['pytest', 'matplotlib'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     author_email='matthew.emery44@gmail.com',
